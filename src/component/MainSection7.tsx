628ddceb432f2591b37bfac9cb508eba9c87014e
junhyok <junhyok153@gmail.com>
2021-05-17 13:41:58 +0900

    🌱손준혁/mainsection6,7

---
+++ src/component/MainSection7.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/MainSection7.scss";
+function MainSection7() {
+  return (
+    <section className="mainSection7">
+      <h2 className="mainSection7Title">
+        사용자들의 솔직한 리뷰를 확인할 수 있습니다
+      </h2>
+      <div>{/* <p>(fake data 넣은 것들을 보여주기)</p> */}</div>
+    </section>
+  );
+}
+
+export default MainSection7;
---
+++ src/page/MainPage.tsx
@@ -0,0 +1,14 @@
+// 세션을 import해주세요
+import MainSection6 from "component/MainSection6";
+import MainSection7 from "component/MainSection7";
+
+function MainPage() {
+  return (
+    <div>
+      <MainSection6></MainSection6>
+      <MainSection7></MainSection7>
+    </div>
+  );
+}
+
+export default MainPage;
