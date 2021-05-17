8496730bb8978f9f8c76e414171d64281cf5797a
codono <codonopsis0130@gmail.com>
2021-05-17 13:36:05 +0900

    🌱 서덕주/메인세션1,2생성

---
+++ src/page/MainPage.tsx
@@ -0,0 +1,14 @@
+import MainSection1 from "component/MainSection1";
+import MainSection2 from "component/MainSection2";
+
+// 세션을 import해주세요
+function MainPage() {
+  return (
+    <>
+      <MainSection1 />
+      <MainSection2 />
+    </>
+  );
+}
+
+export default MainPage;
