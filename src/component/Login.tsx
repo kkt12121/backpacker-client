04e583f64640009e042283a2bb14a3769aaabf72
codono <codonopsis0130@gmail.com>
2021-05-19 15:09:02 +0900

    🌱 서덕주/로그인,유저정보찾기 수정

--- src/component/Login.tsx
+++ src/component/Login.tsx
@@ -1,6 +1,7 @@
 import { getLoginToken } from "action/LoginAction";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import "../css/Login.scss";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -23,21 +24,31 @@ function Login() {
 
   return (
     <>
-      <div>
-        <span>이메일</span>
-        <input className="emailInput" onChange={inputEmailHandler}></input>
-        <span>패스워드</span>
-        <input
-          className="passwordInput"
-          type="password"
-          onChange={inputPwHandler}
-        ></input>
-      </div>
-      <a>이메일 / 패스워드 찾기</a>
-      <div>
-        <button onClick={() => dispatch(getLoginToken(email, password))}>
-          로그인
-        </button>
+      <div className="loginBox">
+        <div className="inputSection">
+          <input
+            className="emailInput"
+            placeholder="이메일"
+            onChange={inputEmailHandler}
+          ></input>
+          <input
+            className="passwordInput"
+            placeholder="패스워드"
+            type="password"
+            onChange={inputPwHandler}
+          ></input>
+          <a className="goToFind" href="http://localhost:3000/finduser">
+            이메일 / 패스워드 찾기
+          </a>
+        </div>
+        <div>
+          <button
+            className="loginBtn"
+            onClick={() => dispatch(getLoginToken(email, password))}
+          >
+            로그인
+          </button>
+        </div>
       </div>
     </>
   );
--- src/component/MainSection2.tsx
+++ src/component/MainSection2.tsx
@@ -8,7 +8,7 @@ function MainSection2() {
     <section className="mainsection2">
       <h2 className="section2text">예산에 맞게 여행계획을 만들어보세요</h2>
       <div className="newcontentgif">
-        <img src="https://media1.tenor.com/images/18a2caf269e1b27a245541ad530bef96/tenor.gif?itemid=8856772"></img>
+        <img src="https://media1.tenor.com/images/2724b55c365d0b4e995c8796606e5e80/tenor.gif?itemid=14625359"></img>
       </div>
     </section>
   );
