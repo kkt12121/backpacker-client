b3fd5827251b1f980d5b0ad0c18254741ca705d0
alta <altanis1223@gmail.com>
2021-05-26 20:13:36 +0900

    🔧홍윤기/컨텐츠타이틀,지역리스트 구현

--- src/component/ContentWriteAreaHeader.tsx
+++ src/component/ContentWriteAreaHeader.tsx
@@ -1,8 +1,10 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import "../css/ContentWriteAreaHeader.scss";
 interface Props {}
 
 export default function ContentWriteAreaHeader({}: Props): ReactElement {
+  const [divMouseOver, setdivMouseOver] = useState(false);
+  const [region, setregion] = useState("");
   return (
     <>
       <div className="contentWriteAreaHeader">
@@ -14,6 +16,131 @@ export default function ContentWriteAreaHeader({}: Props): ReactElement {
           </div>
         </div>
       </div>
+      <span
+        className="regionChoice"
+        onMouseOut={() => {
+          setdivMouseOver(false);
+        }}
+        onMouseOver={() => {
+          setdivMouseOver(true);
+        }}
+      >
+        {region.length === 0 ? "지역을 선택해주세요" : region}
+        <ul className={divMouseOver ? "regionList" : "regionListOff"}>
+          <li
+            onClick={() => {
+              setregion("서울");
+            }}
+          >
+            서울
+          </li>
+          <li
+            onClick={() => {
+              setregion("경기도");
+            }}
+          >
+            경기도
+          </li>
+          <li
+            onClick={() => {
+              setregion("인천");
+            }}
+          >
+            인천
+          </li>
+          <li
+            onClick={() => {
+              setregion("강원도");
+            }}
+          >
+            강원도
+          </li>
+          <li
+            onClick={() => {
+              setregion("충청북도");
+            }}
+          >
+            충청북도
+          </li>
+          <li
+            onClick={() => {
+              setregion("충청남도");
+            }}
+          >
+            충청남도
+          </li>
+          <li
+            onClick={() => {
+              setregion("대전");
+            }}
+          >
+            대전
+          </li>
+          <li
+            onClick={() => {
+              setregion("전라북도");
+            }}
+          >
+            전라북도
+          </li>
+          <li
+            onClick={() => {
+              setregion("전라남도");
+            }}
+          >
+            전라남도
+          </li>
+          <li
+            onClick={() => {
+              setregion("광주");
+            }}
+          >
+            광주
+          </li>
+          <li
+            onClick={() => {
+              setregion("경상북도");
+            }}
+          >
+            경상북도
+          </li>
+          <li
+            onClick={() => {
+              setregion("경상남도");
+            }}
+          >
+            경상남도
+          </li>
+          <li
+            onClick={() => {
+              setregion("대구");
+            }}
+          >
+            대구
+          </li>
+          <li
+            onClick={() => {
+              setregion("울산");
+            }}
+          >
+            울산
+          </li>
+          <li
+            onClick={() => {
+              setregion("부산");
+            }}
+          >
+            부산
+          </li>
+          <li
+            onClick={() => {
+              setregion("제주");
+            }}
+          >
+            제주
+          </li>
+        </ul>
+      </span>
     </>
   );
 }
